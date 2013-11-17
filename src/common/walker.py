093291d34ede5c4ccb97e43a61e4b37d791c77bb
Anatolii Koval <weralwolf@gmail.com>
2013-11-18 01:55:31 +0200

    Adding files one by one

--- src/common/walker.py
+++ src/common/walker.py
@@ -13,27 +13,23 @@ def check_dir(directory_list, file_type, root):
     return queue
 
 
-def write_data_chunk(data):
-    log.debug("Writing %i models..." % len(data))
-    session = db.session()
-    for row in data:
-        try:
-            log.debug('Adding `%s`' % row.filename)
-            session.add(row)
+def write_data_chunk(data, flag):
+    try:
+        session = db.session()
+        log.debug('(%s) Adding `%s`' % (flag, data.filename))
+        session.add(data)
+        session.commit()
+        session.close()
 
-        except:
-            log.error('Error adding file `%s`', row.filename)
-            row.success = False
-            row.clear_data()
+    except:
+        log.error('Error adding file `%s`', data.filename)
+        data.success = False
+        data.clear_data()
 
-            #session = db.session()
-            session.add(row)
-            #session.commit()
-            #session.close()
-    log.debug("Commit short session...")
-    session.commit()
-    session.close()
-    log.debug("Session commited.")
+        session = db.session()
+        session.add(data)
+        session.commit()
+        session.close()
 
 
 def walk(path, file_type, recursive, parser):
@@ -49,19 +45,7 @@ def walk(path, file_type, recursive, parser):
     queue_len = len(queue)
     log.info('%i files collected for parsing...' % queue_len)
 
-    parsed_data = []
-
     data_index = 0
     for i in queue:
-        if len(parsed_data) > 5:
-            log.debug("%i/%i files done" % (data_index, queue_len))
-            write_data_chunk(parsed_data)
-            del parsed_data
-            parsed_data = []
-
-        parsed_data.extend(parser.parse(i))
         data_index += 1
-
-    if len(parsed_data):
-        write_data_chunk(parsed_data)
-
+        write_data_chunk(parser.parse(i)[0], "%i/%i" % (data_index, queue_len))
